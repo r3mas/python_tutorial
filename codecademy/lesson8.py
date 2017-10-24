0daa850645c7a4847844ce1b5e084485b320caa6
remigiusz.glowacki <remigiusz.glowacki@yoti.com>
2017-10-25 00:16:07 +0100

    Lest do Lesson nr 8.3

--- codecademy/lesson8.py
+++ codecademy/lesson8.py
@@ -1,4 +1,21 @@
-"""count = 0
+##8.3
+
+num = 1
+
+while num <=10:
+    print num ** 2
+    num += 1
+
+"""
+#8.2
+loop_condition = True
+
+while loop_condition:
+    print "I am a loop"
+    loop_condition = False
+
+#8.1
+count = 0
 
 if count < 5:
     print "Hello, I am an if statement and count is", count
@@ -6,10 +23,5 @@ if count < 5:
 while count <= 9:
     print "Hello, I am a while and count is", count
     count += 1
+
 """
-
-loop_condition = True
-
-while loop_condition:
-    print "I am a loop"
-    loop_condition = False
