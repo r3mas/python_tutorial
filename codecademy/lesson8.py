30e97fc8a52d818cfb019021306aed574e33d506
remigiusz.glowacki <remigiusz.glowacki@yoti.com>
2017-10-25 01:11:01 +0100

    Lest do Lesson nr 8.13-17

--- codecademy/lesson8.py
+++ codecademy/lesson8.py
@@ -1,3 +1,42 @@
+#8.17
+fruits = ['banana', 'apple', 'orange', 'tomato', 'pear', 'grape']
+
+print 'You have...'
+for f in fruits:
+    if f == 'tomato':
+        print 'A tomato is not a fruit!' # (It actually is.)
+        break
+    print 'A', f
+else:
+    print 'A fine selection of fruits!'
+
+
+"""
+#8.16
+
+list_a = [3, 9, 17, 15, 19]
+list_b = [2, 4, 8, 10, 30, 40, 50, 60, 70, 80, 90]
+
+for a, b in zip(list_a, list_b):
+    print max(a,b)
+
+#8.15
+choices = ['pizza', 'pasta', 'salad', 'nachos']
+
+
+print 'Your choices are:'
+for index, item in enumerate(choices):
+    print index + 1, item
+
+#8.14
+
+d = {'a': 'apple', 'b': 'berry', 'c': 'cherry'}
+
+for key in d:
+    # Your code here!
+    print key, d[key]
+
+
 #8.13
 numbers  = [7, 9, 12, 54, 99]
 
@@ -9,7 +48,7 @@ for num in numbers:
 for i in numbers:
     print i ** 2
 
-"""
+
 #8.12
 
 phrase = "A bird in the hand..."
