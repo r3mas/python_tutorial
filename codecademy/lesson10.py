179c190ef572f265a0a12065dc44dd8dcaa8d207
remigiusz.glowacki <remigiusz.glowacki@yoti.com>
2018-01-18 17:01:52 +0000

    Lessson 10 starts now

---
+++ codecademy/lesson10.py
@@ -0,0 +1,12 @@
+my_dict = {
+    "Name": "Guido",
+    "Age": 56,
+    "BDFL": True,
+    "Lazy": True
+}
+
+print my_dict.keys()
+print my_dict.values()
+
+for key in my_dict:
+    print key,my_dict[key]
